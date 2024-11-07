7c890efa50f17754fac91a906c9d5bfb37b306be
Matías Castillo-Aguilar <matcasti@umag.cl>
2024-11-07 12:11:34 -0300

    analysis script ready

--- R/7-meta-analysis.R
+++ R/7-meta-analysis.R
@@ -13,66 +13,82 @@ m_data <- rri_data[!is.na(rri_std)]
 ## Load adjusted model parameters
 id_data <- fread(file = "models/id_level_posterior.csv")
 
+## Custom functions
+fit_model <- function(model, priors) {
+  file = paste0("models/group_level/",
+                deparse(substitute(model)),
+                ".RDS")
+  
+  if (file.exists(file)) {
+    m_fit <- readRDS(file)
+  } else {
+    m_fit <- brms::brm(formula = model,
+                       data = id_data,
+                       prior = priors,
+                       family = gaussian(),
+                       chains = 5, cores = 5, seed = 1234,
+                       iter = 10000, warmup = 5000,
+                       control = list(adapt_delta = .99,
+                                      max_treedepth = 50))
+    
+    saveRDS(object = m_fit, file = file, compress = "xz")
+  }
+  
+  return(m_fit)
+}
+
 
 # Fit model ---------------------------------------------------------------
 
+## Models
+m_alpha_model <- bf(alphaMu | se(alphaSE, sigma = TRUE) ~ 1 + (1 | id))
+m_beta_model <- bf(betaMu | se(betaSE, sigma = TRUE) ~ 1 + (1 | id))
+m_c_model <- bf(cMu | se(cSE, sigma = TRUE) ~ 1 + (1 | id))
+m_lambda_model <- bf(lambdaMu | se(lambdaSE, sigma = TRUE) ~ 1 + (1 | id))
+m_phi_model <- bf(phiMu | se(phiSE, sigma = TRUE) ~ 1 + (1 | id))
+m_tau_model <- bf(tauMu | se(tauSE, sigma = TRUE) ~ 1 + (1 | id))
+m_delta_model <- bf(deltaMu | se(deltaSE, sigma = TRUE) ~ 1 + (1 | id))
+m_sigma_model <- bf(sigmaMu | se(sigmaSE, sigma = TRUE) ~ 1 + (1 | id))
 
-m_model <- bf(alphaMu | se(alphaSE, sigma = TRUE) ~ 1 + (1 | id)) + 
-  bf(betaMu | se(betaSE, sigma = TRUE) ~ 1 + (1 | id)) + 
-  bf(cMu | se(cSE, sigma = TRUE) ~ 1 + (1 | id)) + 
-  bf(lambdaMu | se(lambdaSE, sigma = TRUE) ~ 1 + (1 | id)) + 
-  bf(phiMu | se(phiSE, sigma = TRUE) ~ 1 + (1 | id)) + 
-  bf(tauMu | se(tauSE, sigma = TRUE) ~ 1 + (1 | id)) + 
-  bf(deltaMu | se(deltaSE, sigma = TRUE) ~ 1 + (1 | id)) +
-  bf(sigmaMu | se(sigmaSE, sigma = TRUE) ~ 1 + (1 | id)) +
-  set_rescor(TRUE)
+# Priors
+m_alpha_prior <-
+  prior(normal(850, 10), class = "Intercept", lb = 0) +
+  prior(normal(115, 10), class = "sigma", lb = 0) +
+  prior(normal(10, 10), class = "sd", lb = 0)
+m_beta_prior <-
+  prior(normal(-400, 50), class = "Intercept", ub = 0) +
+  prior(normal(128, 10), class = "sigma", lb = 0) +
+  prior(normal(50, 10), class = "sd", lb = 0)
+m_c_prior <-
+  prior(normal(0.8, 0.05), class = "Intercept", lb = 0) +
+  prior(normal(0.1, 0.01), class = "sigma", lb = 0) +
+  prior(normal(0.1, 0.05), class = "sd", lb = 0)
+m_lambda_prior <-
+  prior(normal(-3.0, 0.1), class = "Intercept", ub = 0) +
+  prior(normal(1, 0.1), class = "sigma", lb = 0) +
+  prior(normal(0.5, 0.5), class = "sd", lb = 0)
+m_phi_prior <-
+  prior(normal(-2.5, 0.1), class = "Intercept", ub = 0) +
+  prior(normal(1, 0.1), class = "sigma", lb = 0) +
+  prior(normal(0.5, 0.5), class = "sd", lb = 0)
+m_tau_prior <-
+  prior(normal(6.5, 0.1), class = "Intercept", lb = 0) +
+  prior(normal(0.8, 0.1), class = "sigma", lb = 0) +
+  prior(normal(0.5, 0.5), class = "sd", lb = 0)
+m_delta_prior <-
+  prior(normal(3.5, 0.5), class = "Intercept", lb = 0) +
+  prior(normal(1.5, 0.1), class = "sigma", lb = 0) +
+  prior(normal(0.5, 0.5), class = "sd", lb = 0)
+m_sigma_prior <-
+  prior(normal(0.3, 0.05), class = "Intercept", lb = 0) +
+  prior(normal(0.05, 0.01), class = "sigma", lb = 0) +
+  prior(normal(0.05, 0.01), class = "sd", lb = 0)
 
-get_prior(m_model, id_data)
-
-priors <- c(
-  ## Main effects
-  prior(normal(850, 10), class = "Intercept", resp = "alphaMu", lb = 0),
-  prior(normal(-400, 50), class = "Intercept", resp = "betaMu", ub = 0),
-  prior(normal(0.8, 0.1), class = "Intercept", resp = "cMu", lb = 0),
-  prior(normal(-3.0, 0.5), class = "Intercept", resp = "lambdaMu", ub = 0),
-  prior(normal(-2.5, 0.5), class = "Intercept", resp = "phiMu", ub = 0),
-  prior(normal(6.5, 0.5), class = "Intercept", resp = "tauMu", lb = 0),
-  prior(normal(3.5, 0.5), class = "Intercept", resp = "deltaMu", lb = 0),
-  prior(normal(0.3, 0.5), class = "Intercept", resp = "sigmaMu", lb = 0),
-  ## Random effects
-  prior(normal(10, 10), class = "sd", resp = "alphaMu", lb = 0),
-  prior(normal(50, 50), class = "sd", resp = "betaMu", lb = 0),
-  prior(normal(0.1, 0.1), class = "sd", resp = "cMu", lb = 0),
-  prior(normal(0.5, 0.5), class = "sd", resp = "lambdaMu", lb = 0),
-  prior(normal(0.5, 0.5), class = "sd", resp = "phiMu", lb = 0),
-  prior(normal(0.5, 0.5), class = "sd", resp = "tauMu", lb = 0),
-  prior(normal(0.5, 0.5), class = "sd", resp = "deltaMu", lb = 0),
-  prior(normal(0.5, 0.5), class = "sd", resp = "sigmaMu", lb = 0)
-)
-
-m_fit <- brm(formula = m_model,
-    data = id_data,
-    prior = priors,
-    family = gaussian(),
-    chains = 5, cores = 5,
-    iter = 10000, warmup = 5000,
-    control = list(adapt_delta = .99,
-                   max_treedepth = 50)
-    )
-
-saveRDS(m_fit, file = "stage_2_model.RDS")
-
-# 
-# as_draws_df(m_fit_alpha) |> 
-#   # tidybayes::spread_draws(
-#   #   r_id[subject,], Intercept
-#   # ) |> 
-#   # transform(id_intercept = Intercept + r_id) |> 
-#   ggplot(aes(x = b_Intercept)) +
-#   ggdist::stat_halfeye(
-#     fill = "lightblue",
-#     density = "unbounded", trim = FALSE) +
-#   scale_y_continuous(expand = c(0.02,0), breaks = NULL, name = NULL) +
-#   scale_x_continuous(expand = c(0.2,0), n.breaks = 6) +
-#   theme_classic() +
-#   labs(x = expression(symbol(E)~"["*alpha*"]"))
+m_alpha_fit <- fit_model(m_alpha_model, m_alpha_prior)
+m_beta_fit <- fit_model(m_beta_model, m_beta_prior)
+m_c_fit <- fit_model(m_c_model, m_c_prior)
+m_lambda_fit <- fit_model(m_lambda_model, m_lambda_prior)
+m_phi_fit <- fit_model(m_phi_model, m_phi_prior)
+m_tau_fit <- fit_model(m_tau_model, m_tau_prior)
+m_delta_fit <- fit_model(m_delta_model, m_delta_prior)
+m_sigma_fit <- fit_model(m_sigma_model, m_sigma_prior)
